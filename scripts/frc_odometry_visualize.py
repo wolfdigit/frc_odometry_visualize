814be210f92298134fb149395b7faa9d09aaeb78
wolfdigit <wolf.digit@gmail.com>
2021-04-22 21:22:39 +0800

    pose

--- scripts/frc_odometry_visualize.py
+++ scripts/frc_odometry_visualize.py
@@ -43,10 +43,19 @@ def buildMarker(id, x, y, z, yaw, pitch, roll, cm, newcome=False):
     marker.pose.position = geometry_msgs.msg.Point(x, y, z)
     return marker
 
+def buildPose(x, y, z, yaw, pitch, roll):
+    q = tf.transformations.quaternion_from_euler(yaw/180.0*math.pi, pitch/180.0*math.pi, roll/180.0*math.pi, 'rzxz')
+    pose = geometry_msgs.msg.PoseStamped()
+    pose.header.frame_id = '/map'
+    pose.header.stamp = rospy.Time.now()
+    pose.pose.position = geometry_msgs.msg.Point(x, y, z)
+    pose.pose.orientation = geometry_msgs.msg.Quaternion(*q)
+    return pose
 
 cm = 75
 odom_pub = rospy.Publisher('drive_odom', visualization_msgs.msg.MarkerArray)
 markerArr = visualization_msgs.msg.MarkerArray()
+pose_pub = rospy.Publisher('drive_odom_pose', geometry_msgs.msg.PoseStamped)
 rate = rospy.Rate(3)
 while not rospy.is_shutdown():
     if len(markerArr.markers)>0:
@@ -54,4 +63,5 @@ while not rospy.is_shutdown():
     else:
         markerArr.markers.append(buildMarker( 0, odom_x.value, odom_y.value, 0, odom_z.value, 0, 0, cm ))
     odom_pub.publish(markerArr)
+    pose_pub.publish(buildPose(odom_x.value, odom_y.value, 0, odom_z.value, 0, 0))
     rate.sleep()
